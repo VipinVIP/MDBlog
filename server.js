4c5bc555a87334e2a102d8105b4c10a8a4776b3c
VipinVIP <kpvipin33@gmail.com>
2021-01-18 17:24:41 +0530

    Created model and passing data to create post

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express =require('express')
+const mongoose=require('mongoose')
 const articleRouter=require('./routes/articles')
 const app=express()
 
+mongoose.connect('mongodb://localhost/blog',{useNewUrlParser:true,useUnifiedTopology:true})
 
 app.set('view engine', 'ejs')
-app.use('/articles',articleRouter)
+
+app.use(express.urlencoded({extended:false}))
+
+
 app.get('/',(req,res)=>{
     const articles=[{
         title:'Initial Article',
@@ -21,5 +26,7 @@ app.get('/',(req,res)=>{
     }]
     res.render('articles/index',{articles:articles})
 })
+
+app.use('/articles',articleRouter)
 
 app.listen(5000)
