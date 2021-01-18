42a7b288f639d2edf90d0223f13db51acb80996c
VipinVIP <kpvipin33@gmail.com>
2021-01-18 11:34:43 +0530

    created containers to display data

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express =require('express')
+const articleRouter=require('./routes/articles')
 const app=express()
 
 
 app.set('view engine', 'ejs')
+app.use('/articles',articleRouter)
 app.get('/',(req,res)=>{
-    res.render('index')
+    const articles=[{
+        title:'Initial Article',
+        createdAt:new Date(),
+        description:'test description'
+
+
+    },{
+        title:'Second Article',
+        createdAt:new Date(),
+        description:'test description 2'
+
+
+    }]
+    res.render('articles/index',{articles:articles})
 })
 
 app.listen(5000)
