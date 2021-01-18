8eeea1a4a2024efd09af2e1552a99f578d9fcaa5
VipinVIP <kpvipin33@gmail.com>
2021-01-18 18:25:09 +0530

    Add readmore option leding to post page

--- routes/articles.js
+++ routes/articles.js
@@ -6,11 +6,13 @@ router.get('/new',(req,res)=>{
     res.render('articles/new',{article:new Article()})
 })
 
-router.get('/:id',(req,res)=>[
-
-])
+router.get('/:id', async (req,res)=>{
+    const article= await Article.findById(req.params.id)
+    if (article==null) res.redirect('/')
+    res.render('articles/show',{article:article})
+})
 router.post('/',async (req,res)=>{
-      const article=new Article({
+      let article=new Article({
           title:req.body.title,
           description:req.body.description,
           markdown:req.body.markdown,
@@ -20,6 +22,7 @@ try{
 article=await article.save()
 res.redirect(`/articles/${article.id}`)
 }catch(e){
+    console.log(e)
 res.render('articles/new',{article:article})
 }
       
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express =require('express')
 const mongoose=require('mongoose')
+const Article=require('./models/article')
 const articleRouter=require('./routes/articles')
 const app=express()
 
@@ -10,20 +11,8 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:false}))
 
 
-app.get('/',(req,res)=>{
-    const articles=[{
-        title:'Initial Article',
-        createdAt:new Date(),
-        description:'test description'
-
-
-    },{
-        title:'Second Article',
-        createdAt:new Date(),
-        description:'test description 2'
-
-
-    }]
+app.get('/',async (req,res)=>{
+    const articles= await Article.find().sort({createdAt:'desc'})
     res.render('articles/index',{articles:articles})
 })
 
