59af26109a32b8aec3a02db082b7782e44c93201
VipinVIP <kpvipin33@gmail.com>
2021-01-19 16:24:54 +0530

    reset paths by slug instead of id

--- models/article.js
+++ models/article.js
@@ -1,10 +1,11 @@
 const mongoose=require('mongoose')
+const marked=require('marked')
+const slugify=require('slugify')
 
 const articleSchema=new mongoose.Schema({
     title:{
         required:true,
         type:String
-
     },
     description:{
         type:String
@@ -17,7 +18,17 @@ const articleSchema=new mongoose.Schema({
         type:Date,
         default:Date.now
 
+    },
+    slug:{
+        type:String,
+        required:true,
+        unique:true
     }
 })
-
+articleSchema.pre('validate',function (next){
+    if(this.title){
+        this.slug=slugify(this.title,{lower:true, strict:true})
+    }
+    next()
+})
 module.exports=mongoose.model('Article',articleSchema)
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express =require('express')
 const mongoose=require('mongoose')
 const Article=require('./models/article')
+const methodOverride=require('method-override')
 const articleRouter=require('./routes/articles')
 const app=express()
 
-mongoose.connect('mongodb://localhost/blog',{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect('mongodb://localhost/blog',{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true})
 
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method'))
 
 
 app.get('/',async (req,res)=>{
